77bffcd87e8c682dcb70cdd048f5db9c6bdee11f
deeyanie04 <dianne2000camu@gmail.com>
2026-03-02 15:36:21 +0800

    Update feature

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:bu_intrapark/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MaterialApp(
+      home: LoadingPage(),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
